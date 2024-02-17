ec4ceabad441c1258355dcd52c17f1bfdc41f63a
Yuji Matsumoto <matsumoto@nulab.com>
2024-02-17 20:06:20 +0900

    refactor run_spec_tests.sh

--- run_spec_tests.sh
+++ run_spec_tests.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+zig build -Doptimize=ReleaseSafe
+
 (cd spec_test; ls *.json) | while read i; do
-    p=`zig build run -Doptimize=ReleaseSafe -- $i 2>&1 | grep 'test pass' | wc -l`; a=`grep -c assert_  spec_test/$i`
+    p=`./zig-out/bin/zwasmi $i 2>&1 | grep 'test pass' | wc -l`
+    a=`grep -c assert_  spec_test/$i`
     echo $i $p $a
 done | awk '{
   printf("%-22s %5d %5d  (%3.1lf%%)\n", substr($1, 0, length($1)-5), $2, $3, 100 * $2 / ($3 == 0 ? 1 : $3));
   p+=$2;
-   t+=$3
+  t+=$3
 }
 END{
   print "------------------------------------------------";
