4b5faf83df69a3bb995e0dfe9c9a8aa86fa3e2cd
Yuji Matsumoto <matsumoto@nulab.com>
2024-02-23 19:05:06 +0900

    check imported types

--- src/runtime/engine.zig
+++ src/runtime/engine.zig
@@ -61,7 +61,7 @@ pub const Engine = struct {
     }
 
     fn loadModule(self: *Self, module: types.Module, module_name: []const u8) (Error || error{OutOfMemory})!*types.ModuleInst {
-        const extern_vals = try self.resolveImports(module.imports, self.allocator);
+        const extern_vals = try self.resolveImports(module, self.allocator);
         const mod_inst = try self.instance.instantiate(module, extern_vals);
         try self.registerModule(mod_inst, module_name);
         return mod_inst;
@@ -76,18 +76,68 @@ pub const Engine = struct {
         return try self.instance.invokeFunctionByAddr(func_addr, args);
     }
 
-    fn resolveImports(self: *Self, imports: []const types.Import, allocator: std.mem.Allocator) error{OutOfMemory}![]const types.ExternalValue {
+    fn resolveImports(self: *Self, module: types.Module, allocator: std.mem.Allocator) (Error || error{OutOfMemory})![]const types.ExternalValue {
+        const imports = module.imports;
         var external_imports = try allocator.alloc(types.ExternalValue, imports.len);
         for (imports, 0..) |imp, i| {
             if (self.mod_insts.get(imp.module_name)) |mod_inst| {
-                for (mod_inst.exports) |exp| {
-                    if (std.mem.eql(u8, imp.name, exp.name)) {
-                        external_imports[i] = exp.value;
-                    }
+                const exp = try findExport(mod_inst.*, imp.name);
+                if (isMatchType(self.instance.store, exp, module, imp.desc)) {
+                    external_imports[i] = exp;
+                } else {
+                    return Error.IncompatibleImportType;
                 }
             }
         }
         return external_imports;
+    }
+
+    fn isMatchType(store: types.Store, exp: types.ExternalValue, module: types.Module, imp: types.ImportDesc) bool {
+        return switch (exp) {
+            .function => |v| imp == .func and isMatchFuncType(store.funcs.items[v].type, module.types[imp.func]),
+            .table => |v| imp == .table and isMatchTableType(store.tables.items[v].type, imp.table),
+            .memory => |v| imp == .memory and isMatchMemType(store.mems.items[v].type, imp.memory),
+            .global => |v| imp == .global and isMatchGlobalType(store.globals.items[v].type, imp.global),
+        };
+    }
+
+    fn isMatchFuncType(a: types.FuncType, b: types.FuncType) bool {
+        return std.mem.eql(types.ValueType, a.parameter_types, b.parameter_types) and
+            std.mem.eql(types.ValueType, a.result_types, b.result_types);
+    }
+
+    fn isMatchTableType(a: types.TableType, b: types.TableType) bool {
+        return a.ref_type == b.ref_type and isValidLimits(a.limits, b.limits);
+    }
+
+    fn isMatchMemType(a: types.MemoryType, b: types.MemoryType) bool {
+        return isValidLimits(a.limits, b.limits);
+    }
+
+    fn isMatchGlobalType(a: types.GlobalType, b: types.GlobalType) bool {
+        return a.mutability == b.mutability and
+            a.value_type == b.value_type;
+    }
+
+    fn isValidLimits(a: types.Limits, b: types.Limits) bool {
+        if (b.max) |b_max| {
+            if (a.max) |a_max| {
+                if (a_max > b_max)
+                    return false;
+            } else {
+                return false;
+            }
+        }
+        return a.min >= b.min;
+    }
+
+    fn findExport(mod_inst: types.ModuleInst, import_name: []const u8) error{UnknownImport}!types.ExternalValue {
+        for (mod_inst.exports) |exp| {
+            if (std.mem.eql(u8, import_name, exp.name)) {
+                return exp.value;
+            }
+        }
+        return Error.UnknownImport;
     }
 };
 
